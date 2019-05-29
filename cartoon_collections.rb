bb91832c04daae65b1d1c9f280636d53bd439146
Angela Lo <angelawenlo@gmail.com>
2019-05-29 21:01:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,3 +23,12 @@ def find_the_cheese(snacks)
     cheese.include?(snack)
   end
 end
+
+#arr = ['You', 'Me', 'I'] I have a huge home, 1, you, 2. me, 3. I live with me
+def my_place(array)
+  new_string = "I have a huge home!"
+  array.each_with_index do |w, index|
+    new_string << " #{index+1}. #{w}"
+end
+arr = ['You', 'Me', 'I'] 
+my_place(arr)
